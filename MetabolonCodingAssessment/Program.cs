fd3ec085a99adaa86071e55acd1a931e24c17576
Anusha Karanth <anushakaranth@yahoo.ca>
2021-02-25 13:36:40 -0500

    Change the ActivityTrackerRSS to accept list of RSS feed URLS for the same company

--- MetabolonCodingAssessment/Program.cs
+++ MetabolonCodingAssessment/Program.cs
@@ -10,16 +10,18 @@ namespace MetabolonCodingAssessment
         static void Main(string[] args)
         {
             RSSFeed rssFeed = new RSSFeed();
-            List<string> companyList = new List<string>();
+            Dictionary<string, IEnumerable<string>> companyList = new Dictionary<string, IEnumerable<string>>();
             int inactivityDays;
 
             //Dictionary Input
-            var companyDict = new Dictionary<string, string>
+            var companyDict = new Dictionary<string, IEnumerable<string>>
             {
-                { "Unrivaled: Long Island Serial Killer", "https://rss.acast.com/unraveled" },
-                {"Morbid: A True Crime Podcast", "https://audioboom.com/channels/4997220.rss"},
-                {"CNN", "http://rss.cnn.com/rss/cnn_topstories.rss" },
-                {"Crime Junkie", "https://feeds.megaphone.fm/ADL9840290619" }
+                { "Unrivaled: Long Island Serial Killer", new List<string>{"https://rss.acast.com/unraveled" } },
+                {"Morbid: A True Crime Podcast", new List<string>{"https://audioboom.com/channels/4997220.rss"} },
+                {"CNN", new List<string>{"http://rss.cnn.com/rss/cnn_topstories.rss","http://rss.cnn.com/rss/money_pf_college.rss" } },
+                {"Crime Junkie", new List<string>{"https://feeds.megaphone.fm/ADL9840290619" } },
+                {"CNBC", new List<string>{ "https://www.cnbc.com/id/100003114/device/rss/rss.html","https://www.cnbc.com/id/100370673/device/rss/rss.html", "https://www.cnbc.com/id/10000108/device/rss/rss.html" } },
+
             };
 
             //Set number of inactivity days
@@ -31,9 +33,14 @@ namespace MetabolonCodingAssessment
             if (companyList.Count > 0)
             {
                 Console.WriteLine("Companies that have no activity in the past " + inactivityDays + " Days:");
-                foreach (string compName in companyList)
+                foreach (KeyValuePair<string, IEnumerable<string>> compName in companyList)
                 {
-                    Console.WriteLine(compName);
+                    Console.WriteLine("\n" + compName.Key);
+                    foreach (string feed in compName.Value)
+                    {
+                        Console.WriteLine("\t" + feed);
+                    }
+                    
                 }
             }
            
